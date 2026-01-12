976b80ee940933237e9bb54fd762aee311629cb8
RahulSingh <chouhanrahul1999@gmail.com>
2026-01-12 20:42:17 +0530

    added and completed hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <main>
       <Navbar />
+      <Hero />
     </main>
   );
 };
