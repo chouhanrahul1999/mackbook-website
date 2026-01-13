bb62195255cd200c4db350d8a3f95e0261c82ec8
RahulSingh <chouhanrahul1999@gmail.com>
2026-01-13 14:04:32 +0530

    created product view section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import ProductView from "./components/ProductView";
 
 const App = () => {
   return (
     <main>
       <Navbar />
       <Hero />
+      <ProductView />
     </main>
   );
 };
